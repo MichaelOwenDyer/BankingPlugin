1c16d38f41e8f184e85da2ee1f1c8d16649525d4
monst <michaeldyer1200@gmail.com>
2021-07-20 18:04:57 +0200

    Improve VisualizationManager

--- src/main/java/com/monst/bankingplugin/external/VisualizationManager.java
+++ src/main/java/com/monst/bankingplugin/external/VisualizationManager.java
@@ -4,16 +4,13 @@ import com.monst.bankingplugin.BankingPlugin;
 import com.monst.bankingplugin.banking.Bank;
 import com.monst.bankingplugin.geo.BlockVector2D;
 import com.monst.bankingplugin.geo.regions.BankRegion;
-import com.monst.bankingplugin.utils.Callback;
 import com.monst.bankingplugin.utils.Pair;
-import com.monst.bankingplugin.utils.QuickMath;
 import com.monst.bankingplugin.utils.Utils;
 import me.ryanhamshire.GriefPrevention.Visualization;
 import me.ryanhamshire.GriefPrevention.VisualizationElement;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 
@@ -21,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class VisualizationManager {
 
@@ -41,16 +39,12 @@ public class VisualizationManager {
     }
 
     private static void visualize(Player p, Collection<BankRegion> bankRegions, VisualizationType type) {
-        Callback<Visualization> callback = Callback.of(visualization -> {
-            visualization.elements.forEach(element -> element.realBlock = element.location.getBlock().getBlockData());
-            Visualization.Apply(p, visualization);
-        });
         Utils.bukkitRunnable(() -> {
             Visualization visualization = new Visualization();
             for (BankRegion region : bankRegions)
                 visualization.elements.addAll(getRegionElements(region, type));
             visualization.elements.removeIf(element -> outOfRange(p.getLocation(), element.location));
-            Callback.callSyncResult(callback, visualization);
+            Visualization.Apply(p, visualization);
         }).runTaskAsynchronously(BankingPlugin.getInstance());
     }
 
@@ -59,23 +53,27 @@ public class VisualizationManager {
         final World world = sel.getWorld();
         List<VisualizationElement> newElements = new ArrayList<>();
 
-        if (sel.isCuboid()) {
-            // Add blocks at vertices
-            sel.getCorners().stream()
-                    .map(location -> new VisualizationElement(location, type.cornerBlockData, null))
-                    .forEach(newElements::add);
+        // Add blocks at vertices
+        sel.getCorners().stream()
+                .map(loc -> new CornerElement(loc, type))
+                .forEach(newElements::add);
 
-            Block min = sel.getMinimumBlock();
-            Block max = sel.getMaximumBlock();
+        if (sel.isCuboid()) {
+
             MinMax[] dimensions = new MinMax[] {
-                    new MinMax(min.getX(), max.getX()),
-                    new MinMax(min.getY(), max.getY()),
-                    new MinMax(min.getZ(), max.getZ())
+                    new MinMax(sel.getMinX(), sel.getMaxX()),
+                    new MinMax(sel.getMinY(), sel.getMaxY()),
+                    new MinMax(sel.getMinZ(), sel.getMaxZ())
             };
 
-            // Add blocks that are directly adjacent to corner blocks
             for (int i = 0; i < 3; i++) {
-                for (int a : new int[] { dimensions[i].getMin() + 1, dimensions[i].getMax() - 1 }) {
+                IntStream.Builder builder = IntStream.builder();
+                // Add blocks that are directly adjacent to corner blocks
+                builder.add(dimensions[i].getMin() + 1).add(dimensions[i].getMax() - 1);
+                // Add blocks that form the lines between the corners in intervals of the integer "step"
+                for (int a = dimensions[i].getMin() + step; a < dimensions[i].getMax() - step / 2; a += step)
+                    builder.accept(a);
+                for (int a : builder.build().toArray()) {
                     for (int b : new int[] { dimensions[(i + 1) % 3].getMin(), dimensions[(i + 1) % 3].getMax() }) {
                         for (int c : new int[] { dimensions[(i + 2) % 3].getMin(), dimensions[(i + 2) % 3].getMax() }) {
                             Location loc = null;
@@ -84,87 +82,52 @@ public class VisualizationManager {
                                 case 1: loc = new Location(world, c, a, b); break;
                                 case 2: loc = new Location(world, b, c, a);
                             }
-                            newElements.add(new VisualizationElement(loc, type.accentBlockData, null));
-                        }
-                    }
-                }
-            }
-
-            // Add blocks that form the lines between the corners in intervals of the integer "step"
-            for (int i = 0; i < 3; i++) {
-                for (int a = dimensions[i].getMin() + step; a < dimensions[i].getMax() - step / 2; a += step) {
-                    for (int b : new int[] { dimensions[(i + 1) % 3].getMin(), dimensions[(i + 1) % 3].getMax() }) {
-                        for (int c : new int[] { dimensions[(i + 2) % 3].getMin(), dimensions[(i + 2) % 3].getMax() }) {
-                            Location loc = null;
-                            switch (i) {
-                                case 0: loc = new Location(world, a, b, c); break;
-                                case 1: loc = new Location(world, c, a, b); break;
-                                case 2: loc = new Location(world, b, c, a);
-                            }
-                            newElements.add(new VisualizationElement(loc, type.accentBlockData, null));
+                            newElements.add(new AccentElement(loc, type));
                         }
                     }
                 }
             }
         } else {
-            // Add blocks at vertices
-            sel.getCorners().stream()
-                    .map(loc -> new VisualizationElement(loc, type.cornerBlockData, null))
-                    .forEach(newElements::add);
+
+            // Construct some helpful arrays of y-values
+            final int[] minMaxYs = new int[] { sel.getMinY() + 1, sel.getMaxY() - 1 };
+            IntStream.Builder yBuilder = IntStream.builder().add(minMaxYs[0]).add(minMaxYs[1]);
+            for (int y = sel.getMinY() + step; y < sel.getMaxY() - (step / 2); y += step)
+                yBuilder.accept(y);
+            final int[] allYs = yBuilder.build().toArray();
 
             List<BlockVector2D> points = sel.getVertices();
-            for (int i = 0; i < points.size(); i++) {
+            for (int vertex = 0; vertex < points.size(); vertex++) {
 
-                BlockVector2D current = points.get(i);
+                BlockVector2D current = points.get(vertex);
 
-                // Add blocks that are immediately vertically adjacent to corner blocks
-                for (int y : new int[] { sel.getMinY() + 1, sel.getMaxY() - 1 }) {
+                for (int y : allYs) {
+                    // Add blocks that are immediately vertically adjacent to corner blocks
+                    // Add blocks that form the vertical lines at the corners in intervals of the integer "step"
                     Location loc = new Location(world, current.getX(), y, current.getZ());
-                    newElements.add(new VisualizationElement(
-                            loc,
-                            type.accentBlockData,
-                            null
-                    ));
-                }
-
-                // Add blocks that form the vertical lines at the corners in intervals of the integer "step"
-                for (int y = sel.getMinY() + step; y < sel.getMaxY() - (step / 2); y += step) {
-                    Location loc = new Location(world, current.getX(), y, current.getZ());
-                    newElements.add(new VisualizationElement(
-                            loc,
-                            type.accentBlockData,
-                            null
-                    ));
+                    newElements.add(new AccentElement(loc, type));
                 }
 
                 // Get the vertex after the current one; will eventually loop back to the first vertex
-                BlockVector2D next = points.get((i + 1) % points.size());
+                BlockVector2D next = points.get((vertex + 1) % points.size());
 
                 int diffX = next.getX() - current.getX();
                 int diffZ = next.getZ() - current.getZ();
-                double distanceToNext = QuickMath.vectorMagnitude(diffX, diffZ);
+                double distanceToNext = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffZ, 2));
 
                 // These two doubles store the direction from the current vertex to the next. Through vector addition they form a diagonal of length 1
                 float unitX = (float) (diffX / distanceToNext);
                 float unitZ = (float) (diffZ / distanceToNext);
 
-                // The following blocks are placed at both minY and maxY
-                for (int y : new int[] { sel.getMinY(), sel.getMaxY() }) {
+                // The following blocks are placed at minY and maxY
+                for (int y : minMaxYs) {
                     // Add the block that is immediately adjacent to the current vertex and pointing in the direction of the next vertex
                     Location unitAway = new Location(world, current.getX() + 0.5 + unitX, y, current.getZ() + 0.5 + unitZ);
-                    newElements.add(new VisualizationElement(
-                            unitAway,
-                            type.accentBlockData,
-                            null
-                    ));
+                    newElements.add(new AccentElement(unitAway, type));
 
                     // Add the block that is immediately adjacent to the next vertex and pointing in the direction of the current vertex
                     Location unitAwayNext = new Location(world, next.getX() + 0.5 - unitX, y, next.getZ() + 0.5 - unitZ);
-                    newElements.add(new VisualizationElement(
-                            unitAwayNext,
-                            type.accentBlockData,
-                            null
-                    ));
+                    newElements.add(new AccentElement(unitAwayNext, type));
                 }
 
                 final double stopAt = distanceToNext - step / 2.0;
@@ -177,12 +140,8 @@ public class VisualizationManager {
                 double nextX = current.getX() + 0.5 + stepX;
                 double nextZ = current.getZ() + 0.5 + stepZ;
                 for (int hop = step; hop < stopAt; hop += step) {
-                    for (int y : new int[] { sel.getMinY(), sel.getMaxY() })
-                        newElements.add(new VisualizationElement(
-                                new Location(world, (int) nextX, y, (int) nextZ),
-                                type.accentBlockData,
-                                null
-                        ));
+                    for (int y : allYs)
+                        newElements.add(new AccentElement(new Location(world, (int) nextX, y, (int) nextZ), type));
                     nextX += stepX;
                     nextZ += stepZ;
                 }
@@ -200,6 +159,18 @@ public class VisualizationManager {
         return x <= currentPos.getBlockX() - range || x >= currentPos.getBlockX() + range
                 || y <= currentPos.getBlockY() - range || y >= currentPos.getBlockY() + range
                 || z <= currentPos.getBlockZ() - range || z >= currentPos.getBlockZ() + range;
+    }
+
+    private static class CornerElement extends VisualizationElement {
+        public CornerElement(Location location, VisualizationType type) {
+            super(location, type.cornerBlockData, location.getBlock().getBlockData());
+        }
+    }
+
+    private static class AccentElement extends VisualizationElement {
+        public AccentElement(Location location, VisualizationType type) {
+            super(location, type.accentBlockData, location.getBlock().getBlockData());
+        }
     }
 
     private static class MinMax extends Pair<Integer, Integer> {
