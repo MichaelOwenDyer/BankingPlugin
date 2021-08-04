cdb8d9667d507a16c5f6be749f5a4fd1ffcda80f
monst <michaeldyer1200@gmail.com>
2021-08-04 20:01:29 +0200

    Comment out ControlUpdate#execute

--- src/main/java/com/monst/bankingplugin/commands/control/ControlUpdate.java
+++ src/main/java/com/monst/bankingplugin/commands/control/ControlUpdate.java
@@ -4,9 +4,8 @@ import com.monst.bankingplugin.BankingPlugin;
 import com.monst.bankingplugin.commands.SubCommand;
 import com.monst.bankingplugin.lang.Message;
 import com.monst.bankingplugin.utils.Permissions;
-import com.monst.bankingplugin.utils.UpdateChecker;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class ControlUpdate extends SubCommand.ControlSubCommand {
 
@@ -34,34 +33,33 @@ public class ControlUpdate extends SubCommand.ControlSubCommand {
             return true;
         }
 
-        // sender.sendMessage(Messages.UPDATE_CHECKING);
-
-        UpdateChecker uc = new UpdateChecker(plugin);
-        UpdateChecker.Result result = uc.check();
-
-        if (result == UpdateChecker.Result.TRUE) {
-            // plugin.setLatestVersion(uc.getVersion());
-            // plugin.setDownloadLink(uc.getLink());
-            // plugin.setUpdateNeeded(true);
-
-            if (sender instanceof Player) {
-                // Utils.sendUpdateMessage(plugin, (Player) sender);
-            } else {
-                // sender.sendMessage(Messages.UPDATE_AVAILABLE);
-            }
-
-        } else if (result == UpdateChecker.Result.FALSE) {
-            // plugin.setLatestVersion("");
-            // plugin.setDownloadLink("");
-            // plugin.setUpdateNeeded(false);
-            // sender.sendMessage(Messages.UPDATE_NO_UPDATE);
-        } else {
-            // plugin.setLatestVersion("");
-            // plugin.setDownloadLink("");
-            // plugin.setUpdateNeeded(false);
-            // sender.sendMessage(Messages.UPDATE_ERROR);
-        }
+        sender.sendMessage(ChatColor.RED + "Sorry! That feature is not yet implemented.");
         return true;
     }
+
+//    sender.sendMessage(Message.UPDATE_CHECKING.translate());
+//
+//    UpdateChecker uc = new UpdateChecker(plugin);
+//    UpdateChecker.Result result = uc.check();
+//
+//        switch (result) {
+//        case TRUE:
+//            plugin.setLatestVersion(uc.getVersion());
+//            plugin.setDownloadLink(uc.getLink());
+//            plugin.setUpdateNeeded(true);
+//            sender.sendMessage(Message.UPDATE_AVAILABLE.with(Placeholder.VERSION).as(uc.getVersion()).translate());
+//            break;
+//        case FALSE:
+//            plugin.setLatestVersion("");
+//            plugin.setDownloadLink("");
+//            plugin.setUpdateNeeded(false);
+//            sender.sendMessage(Message.UPDATE_NO_UPDATE.translate());
+//        case ERROR:
+//            plugin.setLatestVersion("");
+//            plugin.setDownloadLink("");
+//            plugin.setUpdateNeeded(false);
+//            sender.sendMessage(Message.UPDATE_ERROR.translate());
+//    }
+//        return true;
 
 }
